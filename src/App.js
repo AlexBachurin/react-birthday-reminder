5d5375c669d684a0069fc0070a76306d435dbdd2
Alex <alexbachurin91@gmail.com>
2021-09-13 23:33:02 +0300

    html/css structure

--- src/App.js
+++ src/App.js
@@ -8,6 +8,33 @@ function App() {
           <h1 className="title">Birthday reminder app</h1>
           <div className="underline"></div>
         </div>
+        <h2 className="birthdays-title">Today birthdays!</h2>
+        <div className="date-container">
+          <span className="date">13</span>
+          <span className="month">September</span>
+          <span className="year">2021</span>
+        </div>
+        <section className="birthdays-section">
+          <article className="birthday">
+            <h4>John wick</h4>
+            <p>21 dec 1992</p>
+          </article>
+          <article className="birthday">
+            <div className="img-container">
+              <img src="https://briefly.ru/static/cache/authors/480/gogol.jpeg?1622718658" alt="gogol" />
+            </div>
+            <h4>John doe</h4>
+            <p>21 dec 1995</p>
+          </article>
+          <article className="birthday">
+            <h4>Sally Smith</h4>
+            <p>30 sep 1982</p>
+          </article>
+          <article className="birthday">
+            <h4>Sally Smith</h4>
+            <p>30 sep 1982</p>
+          </article>
+        </section>
       </section>
     </main>
   );
